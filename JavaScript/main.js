d7a56b710cb3950f794044c41feeff242166744f
Braulio Lazo <yobraulio23@gmail.com>
2023-04-26 20:39:36 -0400

    Deploy Congiguration READY

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -11,6 +11,16 @@ function startAudioHub() {
       const sectionADDalbum = document.querySelector("#audiohub__section__addalbum");
       deploySection(sectionADDalbum);
    };
+
+   document.querySelector(".tools__icon__container").onclick = () => {
+      const sectionConfiguration = document.querySelector(".audiohub__section__configuration");
+      deploySection(sectionConfiguration);
+   };
+
+   document.querySelector("#close__configuration").onclick = () => {
+      const sectionConfiguration = document.querySelector(".audiohub__section__configuration");
+      deploySection(sectionConfiguration);
+   };
 }
 
 
