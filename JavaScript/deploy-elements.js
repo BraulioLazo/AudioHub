27ed0dee22e7af650c48ca44a6264b82fa3eb19b
Braulio Lazo <yobraulio23@gmail.com>
2023-04-29 18:51:56 -0400

    button full screen ready

--- JavaScript/deploy-elements.js
+++ JavaScript/deploy-elements.js
@@ -4,3 +4,14 @@ function deploySection(section) {
     section.classList.toggle("deploy__section");
 }
 
+
+function activeSwitch() {
+  const switchBTN = document.querySelectorAll(".switch");
+  switchBTN.forEach(function (element, position) {
+    element.addEventListener("click", ()=>{
+      element.classList.toggle("move__switch")
+    })
+  });
+}
+
+activeSwitch();
